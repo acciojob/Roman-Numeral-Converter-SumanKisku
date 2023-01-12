5a12e1c0ef9e4cfe4f19f454d69bcbabcd4d7c0c
Suman Kisku <sumankisku1@gmail.com>
2023-01-12 23:50:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,7 +10,16 @@ function convertToRoman(num) {
     };
 
   //your code here
+	let romanized = "";
+	const decimalKeys = Objec.keys(numerals).reverse();
 
+	decimalKeys.forEach(key=> {
+		while(key <= num) {
+			romanized += numerals[key];
+			num -= key;
+		}
+	})
+	return romanized;
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
